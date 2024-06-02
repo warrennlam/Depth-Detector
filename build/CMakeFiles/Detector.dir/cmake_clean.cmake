a60d4b94c26741d3f6403ae57453c9590b733861
Warren Lam <warrenlam@Warrens-MacBook-Pro.local>
2024-06-02 15:32:06 -0500

    initial commit

---
+++ build/CMakeFiles/Detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Detector.dir/DepthDetector.cpp.o"
+  "CMakeFiles/Detector.dir/DepthDetector.cpp.o.d"
+  "Detector"
+  "Detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/DepthDetector.cpp.o"
+  "CMakeFiles/MyProject.dir/DepthDetector.cpp.o.d"
+  "MyProject"
+  "MyProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
