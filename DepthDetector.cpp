2dd96a8ddd7454e84094fc2ca05b95a14e7d4e81
Warren Lam <warrenlam@Warrens-MacBook-Pro.local>
2024-06-05 21:55:50 -0500

    added contours

--- DepthDetector.cpp
+++ DepthDetector.cpp
@@ -62,54 +62,18 @@ int main(int, char **)
         bitwise_not(outputImg, outputImg);
 
         // New
-
-        SimpleBlobDetector::Params params;
-
-        // Change thresholds
-        params.filterByColor = true;
-        params.minThreshold = 0;
-        params.maxThreshold = 50;
-
-        // Filter by Area.
-        params.filterByArea = true;
-        params.minArea = 500;
-        // params.maxArea = 5000;
-
-        // Filter by Circularity
-        params.filterByCircularity = true;
-        params.minCircularity = 0.5;
-        params.maxCircularity = 0.9;
-
-        // Filter by Convexity
-        params.filterByConvexity = false;
-        params.minConvexity = 0.87;
-
-        // Filter by Inertia
-        params.filterByInertia = false;
-        params.minInertiaRatio = 0.2;
+        vector<vector<Point>> contours0;
+                vector<Point> hierarchy;
 
 
-
-        // Set up detector with params
-        Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
-
-        // SimpleBlobDetector::create creates a smart pointer.
-        // So you need to use arrow ( ->) instead of dot ( . )
-        // detector->detect( im, keypoints);
-
-        std::vector<KeyPoint> keyPoints;
-
-        detector->detect(outputImg, keyPoints);
-
-        Mat displayKeyPts;
-        drawKeypoints(outputImg, keyPoints, displayKeyPts, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-
-
+        findContours(outputImg, contours0, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+        drawContours( outputImg, outputImg, 2, Scalar (255,0,0), 2, LINE_8, hierarchy, 0 );
+ 
 
         // End New
 
         imshow("Filtered", outputImg);
-        imshow("Normal", displayKeyPts);
+        imshow("Normal", frame);
 
         if (waitKey(5) >= 0)
             break;
