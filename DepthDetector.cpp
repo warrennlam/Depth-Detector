d545d467c8a4c06ea710f356d5d6be97a55a1132
Warren Lam <warrenlam@Warrens-MacBook-Pro.local>
2024-06-05 21:40:22 -0500

    detect blobs

--- DepthDetector.cpp
+++ DepthDetector.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include "opencv2/imgcodecs.hpp"
+#include <opencv2/features2d.hpp>
 #include <iostream>
 #include <stdio.h>
 
@@ -58,8 +59,57 @@ int main(int, char **)
         Mat outputImg;
         outputImg = process.HSVConverter(blured);
 
+        bitwise_not(outputImg, outputImg);
+
+        // New
+
+        SimpleBlobDetector::Params params;
+
+        // Change thresholds
+        params.filterByColor = true;
+        params.minThreshold = 0;
+        params.maxThreshold = 50;
+
+        // Filter by Area.
+        params.filterByArea = true;
+        params.minArea = 500;
+        // params.maxArea = 5000;
+
+        // Filter by Circularity
+        params.filterByCircularity = true;
+        params.minCircularity = 0.5;
+        params.maxCircularity = 0.9;
+
+        // Filter by Convexity
+        params.filterByConvexity = false;
+        params.minConvexity = 0.87;
+
+        // Filter by Inertia
+        params.filterByInertia = false;
+        params.minInertiaRatio = 0.2;
+
+
+
+        // Set up detector with params
+        Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+
+        // SimpleBlobDetector::create creates a smart pointer.
+        // So you need to use arrow ( ->) instead of dot ( . )
+        // detector->detect( im, keypoints);
+
+        std::vector<KeyPoint> keyPoints;
+
+        detector->detect(outputImg, keyPoints);
+
+        Mat displayKeyPts;
+        drawKeypoints(outputImg, keyPoints, displayKeyPts, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+
+
+        // End New
+
         imshow("Filtered", outputImg);
-        imshow("Normal", frame);
+        imshow("Normal", displayKeyPts);
 
         if (waitKey(5) >= 0)
             break;
